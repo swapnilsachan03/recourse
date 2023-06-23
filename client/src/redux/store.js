91b9c33ca64ef69d16691edfcd16afd08bf249a1
swapnil-sachan <swapnilsachan03@gmail.com>
2023-06-23 12:34:32 +0530

    Updated server URL

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -15,4 +15,4 @@ const store = configureStore({
 })
 
 export default store;
-export const server = "http://localhost:4000/api/v1";
+export const server = "https://recourse-backend.vercel.app//api/v1";
